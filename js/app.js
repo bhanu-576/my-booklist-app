b9d86ab168b5000c5302cd85cc66127e2a716dbe
bhanu <bhanusharma576@gmail.com>
2023-08-30 18:17:18 +0530

    delete button is working

--- js/app.js
+++ js/app.js
@@ -1,48 +1,45 @@
 // Book class:represents a book
-
-class Book{
-    constructor(title,author,isbn){
-        this.title=title;
-        this.author = author;
-        this.isbn = isbn;
-    }
-}
-
-// UI class :Handle UI tasks
-
+        class Book{
+            constructor(title,author,isbn){
+                this.title=title;
+                this.author = author;
+                this.isbn = isbn;
+            }
+        }
+        
 
 class UI {
-    static displayBooks(){
-        const storeBooks = [
-            {
-                title : "raat ki raani",
-                author : "Avhishek Bhardwaj",
-                isbn : "12526"
-            },
-            {
-                title : "wo undekhi raat",
-                author : "Avhishek Bhardwaj",
-                isbn : "12527"
-            },
-            {
-                title : "First Kiss (mehak)",
-                author : "Avhishek Bhardwaj",
-                isbn : "12528"
-            },
+    // static displayBooks(){
+    //     const storeBooks = [
+    //         {
+    //             title : "raat ki raani",
+    //             author : "Avhishek Bhardwaj",
+    //             isbn : "12526"
+    //         },
+    //         {
+    //             title : "wo undekhi raat",
+    //             author : "Avhishek Bhardwaj",
+    //             isbn : "12527"
+    //         },
+    //         {
+    //             title : "First Kiss (mehak)",
+    //             author : "Avhishek Bhardwaj",
+    //             isbn : "12528"
+    //         },
            
-        ];
+    //     ];
 
-        const books = storeBooks;
+    //     const books = storeBooks;
 
-        books.forEach((book)=> UI.addBookToList(book));
-    }
+    //     books.forEach((book)=> UI.addBookToList(book));
+    // }
 
   static addBookToList(book)
   {
     const list = document.querySelector('#book-list');
     const row = document.createElement('tr');
    
-    
+
     row.innerHTML = `
     <td>${book.title}</td>
     <td>${book.author}</td>
@@ -50,9 +47,21 @@ class UI {
     <td><a href="#" class = "btn btn-danger btn-sm delete">X</a></td>
     <td><a href="#" class = "btn btn-danger btn-sm edit"><i class = "fas fa-edit"></i></a></td>    
     `;
-    console.log("huhu");
-    // list.appendChild(row);
     list.appendChild(row);
+  }
+
+  //delete book
+  static deleteBook(el){
+    if(el.classList.contains('delete')){
+        el.parentElement.parentElement.remove();
+    }
+  }
+  //clear fields
+  static clearFields(){
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#isbn').value = '';
+
   }
 
   }
@@ -66,11 +75,41 @@ document.addEventListener('DOMContentLoaded',UI.displayBooks);
  
 //Event : Add a book
 
+document.getElementById('book-form').addEventListener('submit',(e)=>{
+        //get the value
+        const title = document.querySelector('#title').value;
+        const author = document.querySelector('#author').value;
+        const isbn = document.querySelector('#isbn').value;
+
+        //validation
+        if(title === '' || author === '' || isbn === ''){
+            alert("Please fill all the fields");
+        } else{
+              //instiate book
+        const book = new Book(title,author,isbn);
+        //add book to UI
+      UI.addBookToList(book);
+
+      //clear fields
+      UI.clearFields();
+      
+
+        }
+
+    
+
+})
+
 
 // Event : Remove a book
+
+document.getElementById('book-list').addEventListener('click',(e)=>{
+    UI.deleteBook(e.target)
+})
 
 //Event : Edit the book
 
 
 
 
+document.querySelector('#title').value = '';
